7be51a42d545ea52c41e3656aafdb500f19b2a38
Nithlesh <39327947+Nithlesh@users.noreply.github.com>
2018-05-22 19:26:29 +0530

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-export CF_API='$1'
+#!/usr/bin/env python
+
+import sys
+import pipes
+
+
+def main(data):
+    print("export %s" % (pipes.quote(str(data))))
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        data = sys.argv[1]
+    else:
+        data = ''
+    main(data)
